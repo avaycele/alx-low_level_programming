e38a3f5599ad92f3cd8644f2e78812151f7f4ef6
avaycele <avaycele@gmail.com>
2023-02-17 15:58:07 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints all the letters except q and e
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

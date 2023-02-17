40c1353f0f1c186ea86133add8a70afbd57a73fd
avaycele <avaycele@gmail.com>
2023-02-17 11:33:33 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *
+ * This function prints a formatted string to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}

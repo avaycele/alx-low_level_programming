a992bc7cb53a41ae1bab3af6b4831b0aac86aff4
avaycele <avaycele@gmail.com>
2023-02-18 10:11:35 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program that prints text, standard error followed by a new line.
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
